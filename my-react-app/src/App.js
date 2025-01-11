038e7910a4d0c2fa4b0e7a04be90a2caeb8c97aa
St Michael <stmichaelegenamba@gmail.com>
2025-01-11 00:57:56 -0800

    Your commit message

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import About from './components/About';
-import Projects from './components/Projects';
-import Exp from './components/Exp';
-import Cert from './components/Cert';
-
-
+import './App.jsx';
 
 
 function App() {
   return (
     <>
-      <NavBar />
-      < About />
-      < Exp />
-      <Projects />
-      <Cert />
-      
-      
-    
+      <App />
     </>
   );
 }
