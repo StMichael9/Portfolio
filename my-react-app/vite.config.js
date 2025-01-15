c9fea19aef8112f2474c2b0161642d77db7a7776
St Michael <stmichaelegenamba@gmail.com>
2025-01-14 18:41:38 -0800

    1/14/2025 6:41PM

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/StMichael.github.io/'
+  base: '/StMichael.github.io/' ,
+  build: {
+    outDir: 'dist'
+  }
 })
